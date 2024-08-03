3b141b9418f041a6040f3fb0f1d85f8db22f42c4
tom <nqwrt@naver.com>
2024-08-03 19:33:09 +0900

    다시 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import Hello from './components/Hello'
 import BootStrapButton from './components/BootstrapExample';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
--- src/components/BootstrapExample.jsx
+++ src/components/BootstrapExample.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 
-
 function BootStrapButton() {
   return (
     <>

e5f3fb9f1a565e3d9f5142783bff5d668837b9b7
TEACHER\ICT02_22 <admin@admin.com>
2024-10-08 17:50:47 +0900

    axios 예제 완료

---
+++ src/components/AxiosExample.jsx
@@ -0,0 +1,63 @@
+import axios from 'axios'
+import React from 'react'
+import { useState } from 'react'
+
+//https://initstory.tistory.com/61
+//https://jsonplaceholder.typicode.com/
+//https://velog.io/@dltmdwls15/Axiospost-jsonplaceholder-post-%EC%97%B0%EC%8A%B5
+const AxiosExample = () => {
+    const [data, setData] = useState([])
+
+    const getClick = () => {
+        axios
+            .get('https://jsonplaceholder.typicode.com/posts')
+            .then((res) => {
+                //handle 성공 했을때
+                setData(res.data)
+            })
+            .catch(function (error) {
+                //handle error
+                console.log(console.error())
+            })
+            .then(function () {
+                console.log('무조건 실행')
+            })
+    }
+
+    const postClick = () => {
+        axios
+            .post('https://jsonplaceholder.typicode.com/posts', {
+                userId: 11,
+                id: 101,
+                body: 'test body',
+                title: 'test title',
+            })
+            .then((res) => console.log(res.data))
+    }
+
+    return (
+        <div>
+            <h2>API 연습</h2>
+            <div>
+                <button onClick={getClick}>Get</button>
+                <button onClick={postClick}>Post</button>
+            </div>
+            {data.map((v, i) => {
+                return (
+                    <>
+                        <div key={i}>
+                            <h3>{v.title}</h3>
+                            <div>
+                                userId = {v.userId}, id = {v.id}
+                            </div>
+                            <div>{v.body}</div>
+                        </div>
+                        <hr></hr>
+                    </>
+                )
+            })}
+        </div>
+    )
+}
+
+export default AxiosExample
--- src/route/Routes.jsx
+++ src/route/Routes.jsx
@@ -1,63 +1,68 @@
-import React from 'react';
-import BoardPage from '../pages/BoardPage';
-import HomePage from '../pages/HomePage';
-import ProfilePage from '../pages/ProfilePage';
-import PropsPage from '../pages/PropsPage';
-import ThreePage from '../pages/ThreePage';
-import UseStatePage from '../pages/UseStatePage';
-import GradePage from '../pages/GradePage';
-import UseRefExamplePage from '../pages/UseRefExamplePage';
-import UseRefDomPage from '../pages/UseRefDomPage';
+import React from 'react'
+import BoardPage from '../pages/BoardPage'
+import HomePage from '../pages/HomePage'
+import ProfilePage from '../pages/ProfilePage'
+import PropsPage from '../pages/PropsPage'
+import ThreePage from '../pages/ThreePage'
+import UseStatePage from '../pages/UseStatePage'
+import GradePage from '../pages/GradePage'
+import UseRefExamplePage from '../pages/UseRefExamplePage'
+import UseRefDomPage from '../pages/UseRefDomPage'
+import AxiosPage from '../pages/AxiosPage'
 
 //React Routes, Route 사용시 배열로 관리하기
 //https://innovatorwhy.tistory.com/12
 const routes = [
-  {
-      path: '/',
-      element: <HomePage />,
-      title:'Home'
-
-  },
-  {
-      path: '/board',
-      element: <BoardPage />,
-      title:'Board'
-  },
-  {
-      path: '/profile',
-      element: <ProfilePage />,
-      title:'Profile'
-  },
-  {
-    path: '/props',
-    element: <PropsPage />,
-    title:'프롭스'
-  },
-  {
-    path: '/thress',
-    element: <ThreePage />,
-    title:'삼항연산자'
-  },
-  {
-    path: '/usestate',
-    element: <UseStatePage />,
-    title:'useState연습'
-  },
-  {
-    path: '/grade',
-    element: <GradePage />,
-    title:'성적표'
-  },
-  {
-    path: '/useref',
-    element: <UseRefExamplePage/>,
-    title:'useRef예제'
-  },
-  {
-    path: '/useref2',
-    element: <UseRefDomPage/>,
-    title:'useRef_Dom_예제'
-  }
-];
+    {
+        path: '/',
+        element: <HomePage />,
+        title: 'Home',
+    },
+    {
+        path: '/board',
+        element: <BoardPage />,
+        title: 'Board',
+    },
+    {
+        path: '/profile',
+        element: <ProfilePage />,
+        title: 'Profile',
+    },
+    {
+        path: '/props',
+        element: <PropsPage />,
+        title: '프롭스',
+    },
+    {
+        path: '/thress',
+        element: <ThreePage />,
+        title: '삼항연산자',
+    },
+    {
+        path: '/usestate',
+        element: <UseStatePage />,
+        title: 'useState연습',
+    },
+    {
+        path: '/grade',
+        element: <GradePage />,
+        title: '성적표',
+    },
+    {
+        path: '/useref',
+        element: <UseRefExamplePage />,
+        title: 'useRef예제',
+    },
+    {
+        path: '/useref2',
+        element: <UseRefDomPage />,
+        title: 'useRef_Dom_예제',
+    },
+    {
+        path: '/axios',
+        element: <AxiosPage />,
+        title: 'axios 예제',
+    },
+]
 
 export default routes

15315529ba6db6a7e8813f2971c32baca0b3ef9a
heo <admin@admin.com>
2025-02-09 16:45:35 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,35 @@
-import BootStrapButton from './components/BootstrapExample';
+import BootStrapButton from './components/BootstrapExample'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import HeaderNav from './components/HeaderNav';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import HeaderNav from './components/HeaderNav'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import routes from './route/Routes';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import routes from './route/Routes'
 
 // https://innovatorwhy.tistory.com/12
 // route 동적 생성
-
-
-
 function App() {
-
-  return (
-    <BrowserRouter>
-      <HeaderNav/>
-      <div className='text-center mt-5'>
-      <Routes>
-        {
-          routes.map((route,index) =>{
-            return(
-              <Route key={route.path || index} path={route.path} element={route.element}>      
-             </Route>
-            );
-          })
-        }
-          {/* <Route path="/" element={<HomePage />} />
+    return (
+        <BrowserRouter>
+            <HeaderNav />
+            <div className="text-center mt-5">
+                <Routes>
+                    {routes.map((route, index) => {
+                        return (
+                            <Route
+                                key={route.path || index}
+                                path={route.path}
+                                element={route.element}
+                            ></Route>
+                        )
+                    })}
+                    {/* <Route path="/" element={<HomePage />} />
           <Route path="/board" element={<BoardPage />} />
           <Route path="/profile" element={<ProfilePage />} /> */}
-      </Routes>
-      </div>
-    </BrowserRouter>    
-  )
+                </Routes>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App
